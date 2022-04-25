3ae7c9e2149fe3ffe4ed82dd9e16b893d8f3df64
Quantum <jakubpiergies@gmail.com>
2022-04-25 20:51:41 +0200

    added handling for new class added with tests purposes in data_base.py module

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from data_base import DataBaseConnection
+'''main module of sending mail message as reminder to return book'''
+from data_base import DataBaseCursor, DataBaseConnection
 from mail_message import MailMessage
 from mail_sending import MailSending
 
-# from data_base
-data_base = DataBaseConnection('data_base.db') # name and directory of data base
+# from data_base - cursor
+data_base_cursor = DataBaseCursor('********.db') #name or directory of data base for cursor
+cursor = data_base_cursor.data_base_con_curs()
+
+# from data_base - data base connection
+
+data_base = DataBaseConnection(cursor)
 data = data_base.data_base_con()
 
 # from mail_message
 message = MailMessage(data)
 mail_message = message.mail_message()
+print(mail_message)
 
 # from mail_sending
-user = '*******@gmail.com'  # sender login
-password = '******' # sender password
-mail_sending = MailSending(user, password, mail_message)
+USER = '*****@****.***'  # sender login
+PASSWORD = '*******' # sender password
+mail_sending = MailSending(USER, PASSWORD, mail_message)
 
 print('-'*15)
 mail_sending.mail_sending()
